2faccb9a2eb708c4b9c3e4037d6fa34afce87d21
rens <rein.satyawarman@gmail.com>
2022-01-21 17:06:16 +0700

    Meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,27 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
-                <title>
-                    rens
-                </title>
+                <title>rens</title>
+                <meta name='title' content='rens' />
+                <meta name='description' content='rens WebApp with NextJS' />
+                <meta property='og:type' content='website' />
+                <meta property='og:url' content='https://metatags.io/' />
+                <meta property='og:title' content='rens' />
+                <meta
+                    property='og:description'
+                    content='rens WebApp with NextJS'
+                />
+                <meta property='og:image' content='/preview.png' />
+                <meta property='twitter:card' content='summary_large_image' />
+                <meta property='twitter:url' content='https://metatags.io/' />
+                <meta property='twitter:title' content='rens' />
+                <meta
+                    property='twitter:description'
+                    content='rens WebApp with NextJS'
+                />
+                <meta property='twitter:image' content='/preview.png' />
             </Head>
-            <Navigator/>
+            <Navigator />
             <Component {...pageProps} />
         </>
     );
